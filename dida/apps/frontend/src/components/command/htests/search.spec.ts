5cc40aeea844b6cf41e38db09528a938fa3c53bd
HSH <335672336@qq.com>
2024-08-21 22:01:19 +0800

    search test

---
+++ dida/apps/frontend/src/components/command/htests/search.spec.ts
@@ -0,0 +1,108 @@
+// 私有方法不用测试！！！！
+
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { useSearch } from '../search'
+
+const searchCommands = vi.fn()
+const resetSearchCommands = vi.fn()
+vi.mock('../searchCommands', () => {
+  return {
+    useSearchCommands() {
+      return {
+        searchCommands,
+        resetSearchCommands,
+      }
+    },
+  }
+})
+
+const searchTasks = vi.fn()
+const resetSearchTasks = vi.fn()
+vi.mock('../searchTasks', () => {
+  return {
+    useSearchTasks() {
+      return {
+        searchTasks,
+        resetSearchTasks,
+      }
+    },
+  }
+})
+
+describe('search', () => {
+  beforeEach(async () => {
+    vi.useFakeTimers()
+    // 在每个测试用例开始前执行以下操作
+    const { resetSearch } = useSearch()
+    // 重置搜索状态，清空搜索内容，重置 loading 和 searching 状态
+    resetSearch()
+    await vi.runAllTimersAsync() // watch 是异步的，，避免多粗调用
+    // 清除所有模拟函数的调用记录和实现,避免 resetSearch 被多次调用
+    vi.clearAllMocks()
+  })
+
+  it('loading should be true when search is not empty', async () => {
+    const { loading, search } = useSearch()
+
+    search.value = 'test'
+
+    // 模拟防抖的定时器
+    vi.useFakeTimers()
+
+    // 等待异步操作完成
+    await vi.advanceTimersToNextTimerAsync()
+
+    expect(loading.value).toBe(true)
+  })
+
+  it('loading should be false when search is completed', async () => {
+    const { loading, search } = useSearch()
+
+    search.value = 'test'
+
+    // 等待异步操作完成
+    await vi.advanceTimersToNextTimerAsync()
+    expect(loading.value).toBe(true)
+    // 等待 handleSearch 执行完成
+    await vi.advanceTimersToNextTimerAsync()
+    expect(loading.value).toBe(false)
+  })
+
+  it('search task', async () => {
+    const { search } = useSearch()
+
+    search.value = 'test'
+
+    await vi.advanceTimersToNextTimerAsync()
+    await vi.runAllTimersAsync()
+    expect(searchTasks).toBeCalledWith('test')
+  })
+
+  it('search command', async () => {
+    const { search } = useSearch()
+
+    search.value = '>test'
+
+    await vi.advanceTimersToNextTimerAsync()
+    await vi.runAllTimersAsync()
+    expect(searchCommands).toBeCalledWith('test')
+  })
+
+  it('should be reset when search is empty', async () => {
+    // 不要这么写，，一个测试中关注点过多，通常一个测试一个关注点
+    const { search, loading, searching } = useSearch()
+
+    search.value = 'test'
+    await vi.advanceTimersToNextTimerAsync()
+    expect(loading.value).toBe(true)
+    await vi.runAllTimersAsync()
+    expect(loading.value).toBe(false)
+    expect(searching.value).toBe(true)
+    search.value = ''
+    await vi.runAllTimersAsync()
+    expect(loading.value).toBe(false)
+    expect(searching.value).toBe(false)
+    expect(resetSearchCommands).toBeCalled()
+    expect(resetSearchTasks).toBeCalled()
+  })
+})
--- dida/apps/frontend/src/components/command/search.ts
+++ dida/apps/frontend/src/components/command/search.ts
@@ -18,25 +18,35 @@ export function useSearch() {
     if (!isInitialized) {
       isInitialized = true
 
+      // 使用 watchDebounced 来监听 search.value 的变化，并添加防抖
       watchDebounced(
         () => search.value,
         async (v) => {
           if (v) {
+            // 设置加载状态为 true
             loading.value = true
+            // 执行搜索操作
             await handleSearch(v)
+            // 搜索完成后，设置加载状态为 false
             loading.value = false
+            // 设置搜索状态为 true，表示搜索已完成
             searching.value = true
           }
         },
-        { debounce: 500 },
+        { debounce: 500 }, // 设置 500ms 的防抖时间
       )
 
+      // 监听搜索值的变化
       watch(
         () => search.value,
         (v) => {
+          // 当搜索值为空字符串时
           if (v === '') {
+            // 重置搜索状态
             resetSearch()
+            // 重置命令搜索
             resetSearchCommands()
+            // 重置任务搜索
             resetSearchTasks()
           }
         },
